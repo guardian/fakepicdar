3a331f414c6ad15a5ace5496c892b0b57d0c50f6
Robert Kenny <robert.kenny@guardian.co.uk>
2016-04-25 16:57:53 +0100

    filter xml files from folder

--- fileloader/fileloader.go
+++ fileloader/fileloader.go
@@ -6,6 +6,8 @@ import (
 	"github.com/guardian/fakepicdar/schema"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 func ReadResponse(filename string) (record schema.Record) {
@@ -27,6 +29,16 @@ func ReadResponse(filename string) (record schema.Record) {
 	return v.ResponseData.Record
 }
 
+func filterXMLFiles(inFiles []os.FileInfo) (outFiles []os.FileInfo) {
+	for _, v := range inFiles {
+		if strings.HasSuffix(v.Name(), ".xml") {
+			outFiles = append(outFiles, v)
+		}
+	}
+
+	return outFiles
+}
+
 func GetRecords(baseDir string, startRange int, endRange int) (record []schema.Record) {
 	len := endRange - startRange
 	records := make([]schema.Record, len)
@@ -37,10 +49,14 @@ func GetRecords(baseDir string, startRange int, endRange int) (record []schema.R
 		log.Fatal(err)
 	}
 
-	currentSlice := files[startRange:endRange]
+	xmlFiles := filterXMLFiles(files)
+	currentSlice := xmlFiles[startRange:endRange]
 
 	for _, file := range currentSlice {
 		filename := xmlDir + "/" + file.Name()
+
+		fmt.Printf("\nLoading %s\n", filename)
+
 		response := ReadResponse(filename)
 
 		records = append(records, response)
